f8092f832e7ab192040796babeaa499b30a4900b
Malachi Irwin <malachi@theirwins.ws>
2018-11-01 08:05:54 -0500

    adapted cards for uno

--- lib/card.rb
+++ lib/card.rb
@@ -1,40 +1,18 @@
 class Card
-  SUITS = {'H' => 'Hearts' ,'D' => 'Diamonds', 'S' => 'Spades', 'C' => 'Clubs'}
-  RANKS = {1 => '2', 2 => '3', 3 => '4', 4 => '5', 5 => '6', 6 => '7', 7 => '8', 8 => '9', 9 => '10', 10 => 'J', 11 => 'Q', 12 => 'K', 13 => 'A'}
-
-  def initialize(suit, rank)
-    @suit = suit
+  def initialize(color, rank)
+    @color = color
     @rank = rank
-  end
-
-  def self.from_value(value)
-    rank, suit = value.split(" of ")
-    card_rank = RANKS.key(rank)
-    card_suit = suit[0]
-    Card.new(card_suit, card_rank)
-  end
-
-  def to_img_path
-    "#{suit.downcase}#{rank_value.downcase}"
-  end
-
-  def to_json(options = {})
-    {rank: rank, rank_value: rank_value, suit: suit, suit_value: SUITS[suit], value: value, to_img_path: to_img_path}.to_json
   end
 
   def rank
     @rank
   end
 
-  def suit
-    @suit
-  end
-
-  def rank_value
-    RANKS[rank]
+  def color
+    @color
   end
 
   def value
-    "#{RANKS[rank]} of #{SUITS[suit]}"
+    "#{color} #{rank}"
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,34 +2,18 @@ require 'rspec'
 require 'card'
 
 describe 'Card' do
-  it 'returns the suit' do
-    card = Card.new('H', 1)
-    expect(card.suit).to eq 'H'
+  it 'returns the color' do
+    card = Card.new('Red', 1)
+    expect(card.color).to eq 'Red'
   end
 
   it 'returns the rank' do
-    card = Card.new('H', 1)
+    card = Card.new('Blue', 1)
     expect(card.rank).to eq 1
   end
 
-  it "goes from a value to a card" do
-    card_value = "4 of Hearts"
-    card = Card.from_value(card_value)
-    expect(card.rank).to eq 3
-    expect(card.suit).to eq "H"
-  end
-
-  it "turns cards to json" do
-    expect(Card.new("H", 5).to_json).to eq "{\"rank\":5,\"rank_value\":\"6\",\"suit\":\"H\",\"suit_value\":\"Hearts\",\"value\":\"6 of Hearts\",\"to_img_path\":\"h6\"}"
-  end
-
-  it "takes a card and turns in into the right thing for finding the card in cards folder" do
-    card = Card.new('H', 10)
-    expect(card.to_img_path).to eq "hj"
-  end
-
-  it "takes a card and turns in into the right thing for finding the card in cards folder" do
-    card = Card.new('H', 2)
-    expect(card.to_img_path).to eq "h3"
+  it 'returns a card value' do
+    card = Card.new('Green', 4)
+    expect(card.value).to eq "Green 4"
   end
 end
